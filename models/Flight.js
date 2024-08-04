f7abd715e1c38affc8d4f845464e68be38f2830f
Leonidas <mishra.prasanna838@gmail.com>
2024-08-04 13:55:00 +0530

    added landing page and flight search

---
+++ models/Flight.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const FlightSchema = new mongoose.Schema({
+    flightNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    from: {
+        type: String,
+        required: true
+    },
+    to: {
+        type: String,
+        required: true
+    },
+    departureTime: {
+        type: Date,
+        required: true
+    },
+    arrivalTime: {
+        type: Date,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Flight', FlightSchema);
